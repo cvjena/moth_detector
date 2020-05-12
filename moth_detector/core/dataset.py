f055a06f6c6b319becb2cd87ce1d1ce6e75b7586
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2020-05-12 23:02:25 +0200

    some fixes in data augmentation

--- moth_detector/core/dataset.py
+++ moth_detector/core/dataset.py
@@ -17,7 +17,7 @@ from matplotlib.patches import Rectangle
 class BBoxDataset(IteratorMixin, BBoxMixin, AnnotationsReadMixin):
 
 	# ImageNet mean (we need this if we use InceptionV3 ???)
-	mean = np.array((123, 117, 104)).reshape((-1, 1, 1))
+	mean = np.array((123, 117, 104), dtype=np.float32).reshape((-1, 1, 1))
 
 	def __init__(self, *args, augment, preprocess, size, center_crop_on_val, **kwargs):
 		super(BBoxDataset, self).__init__(*args, **kwargs)
@@ -26,6 +26,7 @@ class BBoxDataset(IteratorMixin, BBoxMixin, AnnotationsReadMixin):
 		self.size = Size(size)
 		self._augment = augment
 		self.coder = None
+		self._pre_rescale = 1000
 		# self.preprocess = preprocess
 		# self.center_crop_on_val = center_crop_on_val
 
@@ -45,16 +46,70 @@ class BBoxDataset(IteratorMixin, BBoxMixin, AnnotationsReadMixin):
 
 		img = img.transpose(2, 0, 1)
 
-		if self._augment and chainer.config.train:
-			img, bbox = self.augment(img, bbox)
 
-		img -= self.mean
+		if self._augment and chainer.config.train:
+			new_img, new_bbox = self.augment(img, bbox)
+			c, bbox_ok = 0, new_bbox.shape == (1,4)
+			while not bbox_ok and c < 5:
+				new_img, new_bbox = self.augment(img, bbox)
+				bbox_ok = new_bbox.shape == (1,4)
+				c += 1
+
+			# if not bbox_ok:
+			# 	print("Showing faulty results...")
+			# 	y0, x0, y1, x1 = bbox[0]
+			# 	fig, axs = plt.subplots(2)
+			# 	axs[0].set_title("Original Image")
+			# 	axs[0].imshow(img.transpose(1,2,0).astype(np.uint8))
+			# 	print(bbox)
+			# 	axs[0].add_patch(Rectangle((x0,y0), x1-x0, y1-y0, fill=False, linewidth=2))
+
+			# 	axs[1].set_title("Resulting Image")
+			# 	axs[1].imshow(new_img.transpose(1,2,0).astype(np.uint8))
+			# 	print(new_bbox)
+
+			# 	plt.show()
+			# 	plt.close()
+
+			img, bbox = new_img, new_bbox
+		else:
+			img, bbox = self.val_augment(img, bbox)
+
+		img = img - self.mean
 		assert bbox.shape == (1, 4), "Ill-formed bounding box!"
 
 		bbox, labels = self.coder.encode(bbox.astype(np.float32), labels)
 		return img, bbox, labels
 
+	def _scale(self, img, bbox, size):
+		_, *old_size = img.shape
+		img = transforms.scale(img, size, fit_short=True)
+		_, *new_size = img.shape
+		bbox = transforms.resize_bbox(bbox, old_size, new_size)
+		return img, bbox
+
+
+	def val_augment(self, img, bbox):
+		# 1. Resizing with keeping the aspect ratio
+		img, bbox = self._scale(img, bbox, min(self.size))
+
+		# 2. Center crop to square final size
+		final_size = tuple(self.size)
+		img, param = transforms.center_crop(img,
+			final_size,
+			return_param=True)
+		bbox = transforms.crop_bbox(bbox,
+			y_slice=param['y_slice'],
+			x_slice=param['x_slice'],
+			allow_outside_center=True)
+
+		return img, bbox
+
+
 	def augment(self, img, bbox):
+		# 0. scale down huge images
+		if max(img.shape) >= self._pre_rescale:
+			img, bbox = self._scale(img, bbox, size=self._pre_rescale)
 
 		# fig, axs = plt.subplots(2)
 
@@ -91,13 +146,10 @@ class BBoxDataset(IteratorMixin, BBoxMixin, AnnotationsReadMixin):
 			allow_outside_center=True)
 
 		# 4. Resizing with keeping the aspect ratio
-		_, *old_size = img.shape
-		img = transforms.scale(img, min(self.size), fit_short=True)
-		_, *new_size = img.shape
-		bbox = transforms.resize_bbox(bbox, old_size, new_size)
-		final_size = tuple(self.size)
+		img, bbox = self._scale(img, bbox, min(self.size))
 
 		# 5. Random crop to square final size
+		final_size = tuple(self.size)
 		img, param = transforms.random_crop(img,
 			final_size,
 			return_param=True)
