1d3eb987e2a8b266a76cc46ce2d187137dd4230b
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-07-05 09:41:09 +0200

    updated sacred config

--- scripts/00_configs/00_sacred_setup.sh
+++ scripts/00_configs/00_sacred_setup.sh
@@ -1,8 +1,15 @@
 
-if [[ -f sacred_creds.sh ]]; then
-	echo "Sacred credentials found; sacred enabled."
-	source sacred_creds.sh
+SACRED_CREDS=${SACRED_CREDS:-$(realpath ../../../sacred/config.sh)}
+
+if [[ -f ${SACRED_CREDS} && -z ${NO_SACRED} ]]; then
+	echo "Sacred credentials found (${SACRED_CREDS}); sacred enabled."
+	source ${SACRED_CREDS}
 else
-	echo "No sacred credentials found; disabling sacred."
+	if [[ ! -f ${SACRED_CREDS} ]]; then
+		echo "No sacred credentials found (${SACRED_CREDS}); disabling sacred."
+	elif [[ ! -z ${NO_SACRED} ]]; then
+		echo "NO_SACRED was set; disabling sacred."
+	fi
+
 	OPTS="${OPTS} --no_sacred"
 fi
